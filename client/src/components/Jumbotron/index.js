59ca4fb29364c2da7d083c0d58aaf15fc33e6a11
k97aw02 <k97aw02@gmail.com>
2020-09-28 20:30:47 -0500

    add image to jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,8 +3,8 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
+      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center", color:"white", backgroundImage:`url(https://freerangestock.com/sample/87961/man-crafting-wood.jpg)`}}
+      className="jumbotron" 
     >
       {children}
     </div>
